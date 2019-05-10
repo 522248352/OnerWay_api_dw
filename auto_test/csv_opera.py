45c64fa14ec44f3f1ef9e164dbc8a0453ad65c60
522248352 <522248352@qq.com>
2019-05-10 11:46:24 +0800

    csv

--- auto_test/csv_opera.py
+++ auto_test/csv_opera.py
@@ -1,12 +1,16 @@
 # coding=utf-8
 import csv
 import os
-
+import pytest
 csv_file = csv.reader(open(r"D:\eclipse-workspace\Tests\Api_TestCase\auto_test\test_case_data\csv.csv", "r"))
 # csv_file_dic = csv.DictReader(fieldnames="D:\csv.csv")
 
 with open(r"D:\eclipse-workspace\Tests\Api_TestCase\auto_test\test_case_data\csv.csv", "r") as filename:
     csv_re = csv.DictReader(filename)
+    print(csv_re)
+    print(type(csv_re))
+    a = []
     for row in csv_re:
         print(row)
-        print(type(row))
+        a.append(row)
+
