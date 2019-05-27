75be8c36b7b5468229901233f5e48c43560deb56
dw <duwan0525@163.com>
2019-05-27 17:24:06 +0800

    csv

---
+++ OnerWay_Api_DW/common/listTest.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import json
+import time
+import logging
+"list 操作"
+
+list_str = ["a","b","c"]
+
+list_str.append("f")    #添加元素到末尾
+print(list_str)
+
+list_str.insert(1,"g")    #添加元素到指定索引位置
+print(list_str)
+
+list_str.pop(1)
+print(list_str)
+
+dic_str = {"xiaoming":75,"xiaohua":82,"xiaocai":60}
+print(dic_str)
+dic_str["xiaoqing"] = 89
+print(dic_str)
+print("xiao" in dic_str)
+print(dic_str.get("ss"))
+print(dic_str.get("xiaohua"))
+
