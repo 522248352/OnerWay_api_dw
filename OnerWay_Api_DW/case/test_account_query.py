1a16bfac69d18e11b100bae219810a17828f27a5
dw <duwan0525@163.com>
2019-05-30 18:42:42 +0800

    logs

---
+++ OnerWay_Api_DW/case/test_account_query.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+from OnerWay_Api_DW.common.getConfig import GetConfigVal
+from OnerWay_Api_DW.common.public_requests import PublicRequest
+import json
+'账号查询'
+
+def test_account_query():
+
+    s_b = "-"*20
+    print(s_b+"账号查询"+s_b)
+    path = "/merchant/show.htm"
+    gcfv = GetConfigVal()
+    hosts = gcfv.getVal("API", "HOSTS")
+    partnerid = gcfv.getVal("API", "PARTNERID")
+    merno = gcfv.getVal("API", "MERNO_NO_USERID")
+
+    params = {'partnerId':partnerid, 'merNo':merno}
+
+    req = PublicRequest()
+    r = req.get_resu(paths=path, data=params)
+    print(json.dumps(r.json(), indent=2, ensure_ascii=False, sort_keys=False))
+    status = r.json()["status"]
+    code = r.json()["code"]
+    assert status == 1 and code == 101
---
+++ OnerWay_Api_DW/case/test_image_upload.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+import json
+from OnerWay_Api_DW.common.getConfig import GetConfigVal
+from OnerWay_Api_DW.common.public_requests import PublicRequest
+'图片上传'
+
+def test_image_upload():
+
+    '''图片上传'''
+
+    s_b = "-"*20
+    print(s_b+"图片上传"+s_b)
+    path = "/upload/image.htm"
+    gcfv = GetConfigVal()
+    hosts = gcfv.getVal("API", "HOSTS")
+    partnerid = gcfv.getVal("API", "PARTNERID")
+    merno = gcfv.getVal("API", "MERNO_NO_USERID")
+
+    params = {'partnerId':partnerid}
+    files = {"image":("278.png", open(r"D:\278.png", "rb"), "image/png", {})}
+
+    req = PublicRequest()
+    r = req.get_resu(paths=path, data=params,file=files)
+    print(json.dumps(r.json(), indent=2, ensure_ascii=False, sort_keys=False))
+
+    status = r.json()["status"]
+    code = r.json()["code"]
+
+    assert status == 1 and code == 10021
