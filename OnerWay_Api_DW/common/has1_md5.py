4f244ebae09b9345d4f7c1a89a627b6a8bad9b82
dw <duwan0525@163.com>
2019-06-06 14:10:24 +0800

    加密

--- OnerWay_Api_DW/common/has1_md5.py
+++ OnerWay_Api_DW/common/has1_md5.py
@@ -17,3 +17,13 @@ def hash_1(str1):
 strtest = "admin123123zxc"
 print(md5(strtest))
 print(hash_1(strtest))
+
+# # python2.7
+# pwd = "xxx" + chr(163) + "fj"
+# checkcode = hashlib.md5(pwd).hexdigest()
+# print checkcode # ea25a328180680aab82b2ef8c456b4ce
+#
+# # python3.6
+# pwd = "xxx" + chr(163) + "fj"
+# checkcode = hashlib.md5(pwd.encode("utf-8")).hexdigest()
+# print(checkcode) # b517e074034d1913b706829a1b9d1b67
